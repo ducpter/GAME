29188161a6db7ebcd4473bdc6db91f4f715047d6
ducpter <ducp96064@gmail.com>
2024-08-02 22:31:18 +0700

    thêm tính năng cột có thể ngẫu nhiên di chuyển tăng độ khó cho game

--- GAME/pipe.cpp
+++ GAME/pipe.cpp
@@ -1,17 +1,29 @@
-#include "pipe.h"
+﻿#include "pipe.h"
 #include "stdio.h"
+#include <vector>
+#include <cstdlib>
 
 vector<position> posPipe;
+vector<int> verticalSpeedPipe;
+vector<int> verticalDirectionPipe;
 
 bool pipe::init()
 {
     posPipe.clear();
+    verticalSpeedPipe.clear();
+    verticalDirectionPipe.clear();
+
     for (signed char i = 0; i < TOTAL_PIPE; i++)
     {
         position temp;
         temp.getPos(SCREEN_WIDTH + i * PIPE_DISTANCE + 350, (rand() % (randMax - randMin + 1)) + randMin);
         posPipe.push_back(temp);
+        int speed = (rand() % 2) ;
+        verticalSpeedPipe.push_back(speed);
+        int direction = (rand() % 2 == 0) ? 1 : -1;
+        verticalDirectionPipe.push_back(direction);
     }
+
     if (isNULL())
     {
         if (Load("res/image/pipe.png", 1))
@@ -49,10 +61,18 @@ void pipe::update()
             {
                 posPipe[i].y = (rand() % (randMax - randMin + 1)) + randMin;
                 posPipe[i].x = posPipe[(i + TOTAL_PIPE - 1) % TOTAL_PIPE].x + PIPE_DISTANCE;
+                //verticalSpeedPipe[i] = (rand() % 3) + 1;
+                verticalSpeedPipe[i] = (rand() % 2);
+                verticalDirectionPipe[i] = (rand() % 2 == 0) ? 1 : -1;
             }
             else
             {
                 posPipe[i].x -= 3;
+                posPipe[i].y += verticalSpeedPipe[i] * verticalDirectionPipe[i];
+                if (posPipe[i].y < randMin || posPipe[i].y > randMax)
+                {
+                    verticalDirectionPipe[i] = -verticalDirectionPipe[i];
+                }
             }
         }
     }
